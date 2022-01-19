eeb04fc5761e1daeadba7c9223e7b04eb3ed09f3
Javier Rodriguez <javicerodriguez@gmail.com>
2022-01-18 21:34:37 -0300

    🔧 Fix anchors

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,16 +4,16 @@ const Footer: React.FC = () => {
   return(
     <footer className='w-full flex flex-col items-center'>
       <p className='flex mt-8 mb-2'>
-        <a href="https://www.linkedin.com/in/rodriguezjavierc/" target="_blank">
+        <a href="https://www.linkedin.com/in/rodriguezjavierc/" target="_blank" rel="noreferrer">
           <img className='w-8 mx-3 grayscale hover:grayscale-0 duration-500 bg-stone-300 p-1 rounded-md shadow-md shadow-stone-300' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
         </a>
-        <a href="mailto:javicerodriguez@gmail.com" target="_blank">
+        <a href="mailto:javicerodriguez@gmail.com" target="_blank" rel="noreferrer">
           <img className='w-8 mx-3 grayscale hover:grayscale-0 duration-500 bg-stone-300 p-1 rounded-md shadow-md shadow-stone-300' src="https://cdn-icons-png.flaticon.com/512/5968/5968534.png" />
         </a>
-        <a href="https://github.com/JaviCeRodriguez/" target="_blank">
+        <a href="https://github.com/JaviCeRodriguez/" target="_blank" rel="noreferrer">
           <img className='w-8 mx-3 grayscale hover:grayscale-0 duration-500 bg-stone-300 p-1 rounded-md shadow-md shadow-stone-300' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
         </a>
-        <a href="https://twitter.com/javicerodriguez" target="_blank">
+        <a href="https://twitter.com/javicerodriguez" target="_blank" rel="noreferrer">
           <img className='w-8 mx-3 grayscale hover:grayscale-0 duration-500 bg-stone-300 p-1 rounded-md shadow-md shadow-stone-300' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" />
         </a>
       </p>
