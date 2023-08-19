7f2caba555f21659d365c078ed2e48e1739d2092
Javier Rodriguez <javicerodriguez@gmail.com>
2023-08-19 15:42:11 -0300

    add: meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
+import type { Metadata } from "next";
 import Presentation from "@/components/custom/presentation";
+
+export const metadata: Metadata = {
+  title: "Javier Rodriguez | Home",
+  description: "Home page",
+  keywords: [
+    "Mate",
+    "Javo",
+    "React",
+    "Typescript",
+    "Python",
+    "Data",
+    "Frontend",
+    "UI",
+  ],
+};
 
 export default function Home() {
   return (
