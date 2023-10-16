c47837c12656bc5533fa16967da88fea394f6a04
Javier Rodriguez <javicerodriguez@gmail.com>
2023-10-16 01:09:33 -0300

    fix: grid and links

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,47 +7,57 @@ import {
 } from "@/components/ui/card";
 import { getPages, notionClient } from "@/utils/notion";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Page() {
   const pages = await getPages();
   console.log(pages);
   return (
-    <div className="flex flex-col">
+    <div className="grid grid-cols-3 grid-flow-row gap-4">
       {pages.map((page) => (
-        <Card key={page.id}>
-          <CardHeader>
-            <div className="relative w-full h-36">
-              <Image
-                src={(page.cover as any).external.url}
-                alt=""
-                fill
-                style={{
-                  objectFit: "cover",
-                  objectPosition: "center",
-                }}
-              />
-            </div>
-            <CardTitle>
-              {(page.properties.Title as any).title[0].plain_text}
-            </CardTitle>
-            <CardDescription>
-              {(page.properties.Description as any).rich_text[0].plain_text}
-
-              <div>
-                {(page.properties.Tags as any).multi_select.map((tag: any) => (
-                  <Badge
-                    key={tag.name}
-                    style={{
-                      backgroundColor: tag.color,
-                    }}
-                  >
-                    {tag.name}
-                  </Badge>
-                ))}
+        <Link
+          href={`/blog/${
+            (page.properties as any).Slug.rich_text[0].plain_text
+          }`}
+          key={page.id}
+        >
+          <Card>
+            <CardHeader>
+              <div className="relative w-full h-36">
+                <Image
+                  src={(page.cover as any).external.url}
+                  alt=""
+                  fill
+                  style={{
+                    objectFit: "cover",
+                    objectPosition: "center",
+                  }}
+                />
               </div>
-            </CardDescription>
-          </CardHeader>
-        </Card>
+              <CardTitle>
+                {(page.properties.Title as any).title[0].plain_text}
+              </CardTitle>
+              <CardDescription>
+                {(page.properties.Description as any).rich_text[0].plain_text}
+
+                <div>
+                  {(page.properties.Tags as any).multi_select.map(
+                    (tag: any) => (
+                      <Badge
+                        key={tag.name}
+                        style={{
+                          backgroundColor: tag.color,
+                        }}
+                      >
+                        {tag.name}
+                      </Badge>
+                    )
+                  )}
+                </div>
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </Link>
       ))}
     </div>
   );
