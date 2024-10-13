30d34cba2919584cc96c2ef68afe4738a44250d7
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-10-13 10:11:06 -0300

    fix: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,24 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   return (
-    <main className="flex flex-col items-center">
-      <Card className="w-full max-w-md">
-        <CardContent className="flex flex-col items-center p-6 space-y-6">
-          <Avatar className="w-24 h-24">
-            <AvatarImage
-              src="/images/javo.jpeg"
-              alt="Profile Picture"
-              className="bg-center bg-contain"
-            />
-            <AvatarFallback>JR</AvatarFallback>
-          </Avatar>
-          <h1 className="text-2xl font-bold text-center">Javier Rodriguez</h1>
-          <p className="text-center text-gray-500">
-            Welcome to my personal website! 🧉
-          </p>
-          <div className="w-full space-y-4">
-            <HomeLinks />
-          </div>
-        </CardContent>
-      </Card>
-    </main>
+    <Card className="w-full max-w-md">
+      <CardContent className="flex flex-col items-center p-6 space-y-6">
+        <Avatar className="w-24 h-24">
+          <AvatarImage
+            src="/images/javo.jpeg"
+            alt="Profile Picture"
+            className="bg-center bg-contain"
+          />
+          <AvatarFallback>JR</AvatarFallback>
+        </Avatar>
+        <h1 className="text-2xl font-bold text-center">Javier Rodriguez</h1>
+        <p className="text-center text-gray-500">
+          Welcome to my personal website! 🧉
+        </p>
+        <div className="w-full space-y-4">
+          <HomeLinks />
+        </div>
+      </CardContent>
+    </Card>
   );
 }
