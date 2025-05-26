66fb8b2548b36ea00dbbc111cdc7edd63f212b9a
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-05-26 00:27:06 -0300

    feat: add card component and enhance index pages with content and translations

--- src/components/ui/language-switcher.tsx
+++ src/components/ui/language-switcher.tsx
@@ -14,13 +14,13 @@ export function LanguageSwitcher({
   currentRoute,
 }: LanguageSwitcherProps) {
   return (
-    <div className="flex items-center gap-2">
-      <Label htmlFor="language-switch" className="text-lg">
+    <div className="flex items-center cursor-pointer">
+      <Label htmlFor="language-switch" className="text-lg cursor-pointer pr-2">
         {currentLang === "es" ? "🇦🇷" : "🇺🇸"}
       </Label>
       <Switch
         id="language-switch"
-        className="data-[state=checked]:bg-blue-500 data-[state=unchecked]:bg-red-500"
+        className="data-[state=checked]:bg-blue-500 data-[state=unchecked]:bg-red-500 cursor-pointer"
         checked={currentLang === "es"}
         onCheckedChange={() => {
           const newLang = currentLang === "es" ? "en" : "es";
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -6,6 +6,34 @@ export const languages = {
 export const defaultLang = "en";
 
 export const ui = {
-  es: {},
-  en: {},
+  es: {
+    "about.title": "🙋🏼 Sobre mí",
+    "about.description": "Líder Técnico Frontend en",
+    "about.content":
+      "Hago frontend principalmente, pero también un poco de backend y análisis de datos.",
+    "tech.title": "💻 Tecnologías",
+    "tech.description": [
+      "Me manejo bien con el ecosistema de",
+      "y lenguajes como",
+      "y",
+    ],
+    "tech.content": ["Aprendiendo sobre tecnologías para backend para", "y"],
+    "contact.title": "📫 Contacto",
+    "contact.description": ["También me encontrás en", "y en"],
+  },
+  en: {
+    "about.title": "🙋🏼 About me",
+    "about.description": "Lead Frontend Developer at",
+    "about.content":
+      "I do frontend mainly, but also a bit of backend and data analysis.",
+    "tech.title": "💻 Technologies",
+    "tech.description": [
+      "I'm good with",
+      "ecosystem, and languages like",
+      "and",
+    ],
+    "tech.content": ["Learning about backend technologies for", "and"],
+    "contact.title": "📫 Contact",
+    "contact.description": ["You can also find me on", "and"],
+  },
 } as const;
